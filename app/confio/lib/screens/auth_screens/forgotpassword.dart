3fac17460129592655231548c2f7a0880f4faa8f
Carlos Guerrero Alvarez <carlos.guerrero.alvarez.25@dartmouth.edu>
2023-08-15 10:02:21 -0400

    Started on the bloc builder and set foundations for forgot password

--- app/confio/lib/screens/auth_screens/forgotpassword.dart
+++ app/confio/lib/screens/auth_screens/forgotpassword.dart
@@ -1,24 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:confio/logic/blocs/forgot_password_bloc/forgot_password_bloc.dart';
 
 class ForgotPasswordScreen extends StatelessWidget {
   const ForgotPasswordScreen({super.key});
 
+	static final _provider = BlocProvider(
+    create: (BuildContext context) => ForgotPasswordBloc(),
+    child: const ForgotPasswordLayout(),
+  );
+
   @override
   Widget build(BuildContext context) {
-		return Scaffold(
-			resizeToAvoidBottomInset: false,
-			body: Center(
-				child: Container(
-					decoration: const BoxDecoration(
-						gradient: LinearGradient(
-							begin: Alignment.bottomCenter,
-							end: Alignment.topCenter,
-            	colors: [Colors.white, Color(0xFF3550DA)],
-							stops: [0.3, 0.7]
-            ),
-					),
-				),
-			)
+		return MaterialApp(
+			home: _provider,
 		);
   }
+}
+
+class ForgotPasswordLayout extends StatelessWidget {
+	const ForgotPasswordLayout({super.key});
+
+	static TextEditingController emailController = TextEditingController();
+
+	@override
+	Widget build(BuildContext context) {
+		return BlocBuilder<ForgotPasswordBloc, ForgotPasswordState>(
+			builder: (context, state) {
+				if (state is ForgotPasswordLoading) {
+					return const Center(
+						child: CircularProgressIndicator()
+					);
+				}	
+				return Scaffold(
+					body: Center(
+						child: Container(
+							decoration: const BoxDecoration(
+								gradient: LinearGradient(
+                  colors: [Colors.white, Color(0xFF3550DA)],
+								),
+							),
+							padding: const EdgeInsets.all(35),
+						)
+					)
+				);
+			},
+		);
+	}
 }
