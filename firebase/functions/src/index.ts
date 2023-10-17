7d5af096d7fdaa6a19b9fbc928e23180ea752a1b
Yamil Quispe <yamiljquispe@gmail.com>
2023-10-17 13:02:16 -0400

    Implement scheduled notifications for due payments

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -8,8 +8,12 @@
  */
 
 import * as functions from "firebase-functions";
-import { firestore } from "firebase-admin";
 import * as admin from "firebase-admin";
+import { messaging } from 'firebase-admin';
+
+// This method initializes the SDK so that it can be used later in the code.
+admin.initializeApp();
+
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
@@ -26,26 +30,34 @@ export const sendNotification = functions.pubsub.schedule('0 8 * * *').onRun(asy
     const date = new Date();
     const db = admin.firestore();
     const querySnapshot = await db.collection('payments').where('due', '==', date).get();
+
+    // Prepare objects to store user tokens and their respective notification data
     const userFromTokens: {[k: string]: any} = {};
     const userToTokens: {[k: string]: any} = {};
+
+    // Process each document from the 'payments' collection
     querySnapshot.forEach(async (doc) => {
         const userToSnapshot = await db.collection('users').doc(doc.data().userTo).get();
         const userFromSnapshot = await db.collection('users').doc(doc.data().userFrom).get();
+
         if (userFromSnapshot.exists && userToSnapshot.exists) {
             const userToData = userToSnapshot.data();
             const userFromData = userFromSnapshot.data();
-            if (userToData) {
-                userToTokens[userToData.token as string] = {
-                    'notification': {
+
+            // Construct notification messages and collect tokens for 'To' users
+            if (userToData?.token) {
+                userToTokens[userToData.token] = {
+                    notification: {
                         title: 'Payment Reminder',
-                        body: 'You have a bill .',
+                        body: 'You have a bill due.',
                         clickAction: 'FLUTTER_NOTIFICATION_CLICK',
                     },
                 };
-                userToTokens[userToData.token].push(userToSnapshot.data()?.token);
             }
-            if (userFromData) {
-                userFromTokens[userFromData.token as string] = {
+
+            // Construct notification messages and collect tokens for 'From' users
+            if (userFromData?.token) {
+                userFromTokens[userFromData.token] = {
                     notification: {
                         title: 'Payment Reminder',
                         body: 'Please pay your bill.',
@@ -56,14 +68,24 @@ export const sendNotification = functions.pubsub.schedule('0 8 * * *').onRun(asy
                         message: 'Please pay your bill.',
                     },
                 };
-                userFromTokens[userFromSnapshot.data()?.token].push(userFromSnapshot.data()?.token);
             }
         }
     });
-    // TODO: send notification to userToTokens and userFromTokens
-    // await admin.messaging().sendToDevice(Object.keys(userToTokens), payload);
-    // await admin.messaging().sendToDevice(Object.keys(userFromTokens), payload);
-    // build the FCM notification payload
-    // await admin.messaging().()
 
-    });
+    // Convert the token maps to arrays of messages
+    const messagesTo = Object.entries(userToTokens).map(([token, message]) => ({...message, token}));
+    const messagesFrom = Object.entries(userFromTokens).map(([token, message]) => ({...message, token}));
+
+    // Send the notifications to the respective users
+    try {
+        // Send notifications to 'To' users
+        const responseTo = await messaging().sendAll(messagesTo);
+        console.log(`${responseTo.successCount} notifications sent successfully to 'To' users`);
+
+        // Send notifications to 'From' users
+        const responseFrom = await messaging().sendAll(messagesFrom);
+        console.log(`${responseFrom.successCount} notifications sent successfully to 'From' users`);
+    } catch (error) {
+        console.error('Error sending notifications:', error);
+    }
+});
